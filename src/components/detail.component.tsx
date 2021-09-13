ece3f120ffe78f97d6200ddb3490d5192d747d29
Mohamed IDRISSI <med.idrissi@outlook.com>
2021-09-13 01:27:21 +0100

    chore: add keys.json file

--- src/components/detail.component.tsx
+++ src/components/detail.component.tsx
@@ -1,5 +1,12 @@
-import { FC } from 'react';
-import { Button, Drawer } from 'rsuite';
+import { FC, useEffect, useState } from 'react';
+import {
+  Button,
+  ControlLabel,
+  Drawer,
+  Form,
+  FormGroup,
+  InputPicker,
+} from 'rsuite';
 import { setCurrentItem } from '~/store/actions.store';
 import { useStore } from '~/store/provider.store';
 
@@ -7,13 +14,35 @@ const Detail: FC = () => {
   const close = () => dispatch(setCurrentItem(undefined));
 
   const [{ current }, dispatch] = useStore();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('keys.json')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  if (!current) {
+    return null;
+  }
 
   return (
     <Drawer show={!!current} onHide={close}>
       <Drawer.Header>
-        <Drawer.Title> Item Detail </Drawer.Title>
+        <Drawer.Title>Item Detail</Drawer.Title>
       </Drawer.Header>
-      <Drawer.Body></Drawer.Body>
+      <Drawer.Body>
+        <Form>
+          <FormGroup>
+            <ControlLabel>Key</ControlLabel>
+            <InputPicker
+              data={data}
+              groupBy="group"
+              value={current.key}
+            ></InputPicker>
+          </FormGroup>
+        </Form>
+      </Drawer.Body>
       <Drawer.Footer>
         <Button onClick={close} appearance="primary">
           Save
